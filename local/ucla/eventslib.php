e7821abbf4a06cc22fa6907aebf49ff8757ffee9
Rex Lorenzo <rex@oid.ucla.edu>
2013-12-06 17:52:00 -0800

    CCLE-4270 - Restore has Duplicate Announcements and Discussion Forums
    * Added event handler to delete duplicate default forums and related unit tests.

--- local/ucla/eventslib.php
+++ local/ucla/eventslib.php
@@ -2,22 +2,107 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->dirroot . '/backup/util/interfaces/checksumable.class.php');
+require_once($CFG->dirroot . '/backup/backup.class.php');
 require_once($CFG->dirroot . '/local/ucla/lib.php');
+require_once($CFG->dirroot . '/mod/forum/lib.php');
 
-function course_restored_enrol_check($data) {
+/**
+ * When a course is restored, check if it has duplicate Announcement
+ *
+ * @param stdClass $data    Event object from restore procedure.
+ * @return boolean
+ */
+function course_restored_dedup_default_forums($data) {
     global $DB;
 
-   // only respond to course restores
+   // Only respond to course restores.
    if ($data->type != backup::TYPE_1COURSE) {
        return true;
    }
 
-    $record = $DB->get_record('enrol', array('enrol' => 'database',
-        'courseid' => $data->courseid, 'status' => ENROL_INSTANCE_DISABLED));
+   // Check if restored course has duplicate "Announcements" and/or
+   // "Discussion forum" forums. Array is indexed as type => default name.
+   $defaultforums = array('news'    => get_string('namenews', 'forum'),
+                          'general' => get_string('discforum', 'format_ucla'));
 
-    if(!empty($record)) {
-        ucla_reg_enrolment_plugin_cron::update_plugin($courseid, $record->id);
+   foreach ($defaultforums as $type => $defaultname) {
+       // Get forum and course module information.
+       $sql = "SELECT   cm.id AS cmid,
+                        f.id AS id,
+                        f.name AS name
+               FROM     {forum} f
+               JOIN     {course_modules} cm ON (f.id=cm.instance)
+               JOIN     {modules} m ON (cm.module=m.id)
+               WHERE    f.course = ? AND
+                        f.type = ? AND
+                        m.name='forum'
+               ORDER BY f.id ASC";
+       if ($forums = $DB->get_records_sql($sql, array($data->courseid, $type))) {
+           if (count($forums) > 1) {
+               $forumdelete = array();
+               /* Try to keep at least 1 default forum type:
+                * 1) Check if forum has any content, if so, then don't delete
+                *    it. Skip to end.
+                * 2) Check if forum has changed its name from the default, if
+                *    so, then don't delete it. Skip to end.
+                * 3) If forum does not have any content or changed its title,
+                *    then mark it for deletion.
+                * 4) If no $forumdelete is the same as the number of $forums,
+                *    then choose the first entry from $forumdelete to keep and
+                *    delete the rest.
+                */
+               foreach ($forums as $forum) {
+                   // Any content?
+                   if ($DB->record_exists('forum_discussions', 
+                           array('forum' => $forum->id))) {
+                       continue;
+                   }
+                   // Changed default name?
+                   if ($defaultname != $forum->name) {
+                       continue;
+                   }
+                   $forumdelete[] = $forum;
+               }
+
+               if (count($forumdelete) == count($forums)) {
+                   // All forums are eligible to be deleted, so keep first.
+                   array_shift($forumdelete);
+               }
+
+               if (!empty($forumdelete)) {                   
+                   foreach ($forumdelete as $todelete) {
+                       // Delete course module entry.
+                       course_delete_module($todelete->cmid);
+                   }
+               }
+           }
+       }
+   }
+
+   return true;
+}
+
+/**
+ * Makes sure that restored courses have the UCLA database enrollment plugin
+ * enabled, if it exists.
+ *
+ * @param stdClass $data    Event object from restore procedure.
+ * @return boolean
+ */
+function course_restored_enrol_check($data) {
+    global $DB;
+
+    // Only respond to course restores.
+    if ($data->type == backup::TYPE_1COURSE) {
+        $record = $DB->get_record('enrol', array('enrol' => 'database',
+            'courseid' => $data->courseid, 'status' => ENROL_INSTANCE_DISABLED));
+        if(!empty($record)) {
+            ucla_reg_enrolment_plugin_cron::update_plugin($data->courseid, $record->id);
+        }
     }
+
+    return true;
 }
 
 /**
