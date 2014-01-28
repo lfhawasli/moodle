eb627ca7fc0d4e0f56d8987889b618a71fcab3c3
Rex Lorenzo <rex@oid.ucla.edu>
2014-01-27 17:15:55 -0800

    CCLE-3101 - Auto-generated forums (Announcements/Discussion) are public by default
    * In CLI script, added ability to process all forums, so that collaboration sites can have their forums fixed as well.

--- local/ucla/scripts/pp_fixforums.php
+++ local/ucla/scripts/pp_fixforums.php
@@ -13,10 +13,12 @@ require_once($CFG->dirroot . '/lib/clilib.php');
 
 list($options, $unrecognized) = cli_get_params(
     array(
-        'help' => false
+        'help' => false,
+        'all' => false
     ),
     array(
-        'h' => 'help'
+        'h' => 'help',
+        'a' => 'all'
     )
 );
 
@@ -26,7 +28,8 @@ if ($options['help']) {
  term.
 
 Options:
--h, --help            Print out this help
+-h, --help          Print out this help
+-a, --all           Process all course forums
 
 Example:
 \$sudo -u www-data /usr/bin/php local/ucla/scripts/pp_fixforums [TERM]
@@ -35,14 +38,19 @@ Example:
     die;
 }
 
-// Make sure that first parameter is a term.
-if (empty($unrecognized) || !ucla_validator('term', $unrecognized[0])) {
-    die("Must pass in a valid term.\n");
+// Check if we are processing all forums or just some for a given term.
+$term = null;
+if ($options['all']) {
+    // Assume we are processing all terms.
+} else {
+    // Validate term.
+    if (empty($unrecognized) || !ucla_validator('term', $unrecognized[0])) {
+        die("Must pass in a valid term.\n");
+    }
+    $term = $unrecognized[0];
 }
-$term = $unrecognized[0];
 
 $params = array(
-    'term' => $term,
     'discussion' => 'Discussion forum',
     'announcement' => 'Announcements',
 );
@@ -54,9 +62,16 @@ $sql = "SELECT  cm.id, urc.courseid, c.shortname, f.name
         JOIN    {course_modules} cm ON (cm.module=m.id AND cm.instance=f.id AND f.course=cm.course)
         JOIN    {course} c ON (cm.course=c.id)
         JOIN    {ucla_request_classes} urc ON (c.id=urc.courseid)
-        WHERE   urc.term=:term AND
-                ((f.type='general' AND f.name LIKE :discussion)
+        WHERE   ((f.type='general' AND f.name LIKE :discussion)
                 OR (f.type='news' AND f.name LIKE :announcement))";
+
+if (!empty($term)) {
+    $sql .= " AND urc.term=:term";
+    $params['term'] = $term;
+    echo "Processing term $term\n";
+} else {
+    echo "Processing all forums\n";
+}
 
 $records = $DB->get_recordset_sql($sql, $params);
 
@@ -80,7 +95,7 @@ if ($records->valid()) {
 
     echo "Made $countforums forums private.\n";
 } else {
-    echo "No records found.  Nothing was modified.\n";
+    echo "No records found. Nothing was modified.\n";
 }
 
 echo "Finished.\n";
