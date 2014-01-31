1a28b0bf0b1e755f26ffa28d417a0322fe09e406
Rex Lorenzo <rex@oid.ucla.edu>
2014-01-31 13:16:45 -0800

    CCLE-4401 - Blackboard collaborate cron failing
    * Using Moodle APIs to get/set config settings.
    * Changing cron run times for plugin to run every 30 minutes instead of every 5 minutes to, hopefully, fix some lingering issues.

--- mod/elluminate/version.php
+++ mod/elluminate/version.php
@@ -1,7 +1,11 @@
 <?php
 $module->requires = 2010112400;
-$module->version  = 2013080201;  // The current module version (Date: YYYYMMDDxx)
-$module->cron     = 600;         // Period for cron to check this module (secs)
+// START UCLA MOD: CCLE-4401 - Blackboard collaborate cron failing
+//$module->version  = 2013080201;  // The current module version (Date: YYYYMMDDxx)
+//$module->cron     = 600;         // Period for cron to check this module (secs)
+$module->version  = 2013080202;  // The current module version (Date: YYYYMMDDxx)
+$module->cron     = 1800;         // Period for cron to check this module (secs)
+// END UCLA MOD: CCLE-4401
 $module->release  = '3.1.0-7';	 // Human Readable version number
 $module->maturity = MATURITY_STABLE;
 $module->component = 'mod_elluminate';
