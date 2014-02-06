1e13f519881fe6360428afa16e2f92d0b8efe60d
Rex Lorenzo <rex@oid.ucla.edu>
2014-02-05 18:35:35 -0800

    TESTINGCCLE-714 - Backup and restore as a new course with private/public material from M2 to M2
    * Added step definitions to check if activity is public/private and to make content public/private.

--- local/publicprivate/tests/behat/behat_publicprivate.php
+++ local/publicprivate/tests/behat/behat_publicprivate.php
@@ -23,21 +23,89 @@ class behat_publicprivate extends behat_base {
     /**
      * Step to test if an activity is private.
      * 
-     * @todo: implement in xpath?
      * @Then /^"([^"]*)" activity should be private$/
      */
-    public function activity_shouldbe_private($activityname) {
-        return new Given("I should see \"$activityname(Private Course Material)\"");
+    public function activity_should_be_private($activityname) {
+        $activitynode = $this->get_activity_node($activityname);
+
+        // Should have "Private Course Material" grouping label.
+        $exception = new ExpectationException('"' . $activityname . '" is not private', $this->getSession());
+        $grouping = $this->find('css', 'span.groupinglabel', $exception, $activitynode);
+        
+        if ($grouping->getText() != "(Private Course Material)") {
+            throw $exception;
+        }
     }
 
     /**
      * Step to test if activity is public.
      * 
-     * @todo: implement in xpath?
      * @Then /^"([^"]*)" activity should be public$/
      */
-    public function activity_shouldbe_public($activityname) {
-        return new Given("I should not see \"$activityname(Private Course Material)\"");
+    public function activity_should_be_public($activityname) {
+        $activitynode = $this->get_activity_node($activityname);
+
+        // Should not have a grouping label.
+        try {
+            $this->find('css', 'span.groupinglabel', $exception, $activitynode);
+            throw new ExpectationException('"' . $activityname . '" is not public', $this->getSession());
+        } catch (ElementNotFoundException $e) {
+            // All ok.
+        }
     }
 
+    /**
+     * Returns the DOM node of the activity from <li>.
+     *
+     * Copied from course/tests/behat/behat_course.php.
+     *
+     * @throws ElementNotFoundException Thrown by behat_base::find
+     * @param string $activityname The activity name
+     * @return NodeElement
+     */
+    protected function get_activity_node($activityname) {
+
+        $activityname = $this->getSession()->getSelectorsHandler()->xpathLiteral($activityname);
+        $xpath = "//li[contains(concat(' ', normalize-space(@class), ' '), ' activity ')][contains(., $activityname)]";
+
+        return $this->find('xpath', $xpath);
+    }
+
+    /**
+     * Make course module private.
+     * 
+     * @Given /^I make "([^"]*)" private$/
+     * @param string $activityname
+     */
+    public function i_make_private($activityname)
+    {
+        $steps = array(
+            new Given('I click on "' . get_string('publicprivatemakeprivate', 'local_publicprivate') . '" "link" in the "' . $this->escape($activityname) . '" activity')
+        );
+
+        if ($this->running_javascript()) {
+            $steps[] = new Given('I wait "2" seconds');
+        }
+
+        return $steps;
+    }
+
+    /**
+     * Make course module public.
+     *
+     * @Given /^I make "([^"]*)" public$/
+     * @param string $activityname
+     */
+    public function i_make_public($activityname)
+    {
+        $steps = array(
+            new Given('I click on "' . get_string('publicprivatemakepublic', 'local_publicprivate') . '" "link" in the "' . $this->escape($activityname) . '" activity')
+        );
+
+        if ($this->running_javascript()) {
+            $steps[] = new Given('I wait "2" seconds');
+        }
+
+        return $steps;
+    }
 }
