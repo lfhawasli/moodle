75b82c6fd7a1254c8cc1855ddc32fc1c460328ae
Rex Lorenzo <rex@oid.ucla.edu>
2014-02-24 17:46:20 -0800

    CCLE-4387 - MyUCLA gradebook transfer chokes on certain feedback
    * Added filtering of invalid ASCII characters in feedback/comments send to MyUCLA.
    * Added related unit tests.

--- local/gradebook/ucla_grade_grade.php
+++ local/gradebook/ucla_grade_grade.php
@@ -1,10 +1,25 @@
 <?php
+// This file is part of the UCLA local gradebook plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once($CFG->libdir . '/grade/grade_grade.php');
 
 class ucla_grade_grade extends grade_grade {
+
     /**
-     * Handler for grade updates.  This should only be called by a grade udpate
+     * Handler for grade updates.  This should only be called by a grade update
      * event handler.  
      * 
      * @global type $CFG
@@ -12,19 +27,19 @@ class ucla_grade_grade extends grade_grade {
      */
     public function webservice_handler() {
         global $CFG;
-        
-        if (!empty($CFG->gradebook_send_updates)) {            
+
+        if (!empty($CFG->gradebook_send_updates)) {
             $result = $this->send_to_myucla();
 
             if ($result !== grade_reporter::SUCCESS &&
                     $result !== grade_reporter::NOTSENT) {
-                // report failure if there was a problem on MyUCLA's end
+                // Report failure if there was a problem on MyUCLA's end
                 // NOTSENT is if a grade item isn't suppose to be sent via
-                // processing on our end
+                // processing on our end.
                 return false;
             }
         }
-        
+
         return true;
     }
 
@@ -39,39 +54,42 @@ class ucla_grade_grade extends grade_grade {
     public function send_to_myucla() {
         global $DB, $CFG;
 
-        // need access to the grade_item object
+        // Need access to the grade_item object.
         if (empty($this->grade_item)) {
             $this->load_grade_item();
         }
 
-        // don't push certain grade types
+        // Don't push certain grade types.
         if ($this->grade_item->itemtype === 'course' ||
                 $this->grade_item->itemtype === 'category') {
             return grade_reporter::NOTSENT;
         }
 
-        // Get crosslisted SRS list
+        // Get crosslisted SRS list.
         $courses = ucla_get_course_info($this->grade_item->courseid);
 
         if (empty($courses)) {
-            // course was not a srs course, so skip it
+            // Course was not a srs course, so skip it.
             return grade_reporter::NOTSENT;
         }
 
-        $srs_list = implode(',', array_map(function($o) {return $o->srs;}, $courses));
+        $srs_list = implode(',',
+                array_map(function($o) {
+                            return $o->srs;
+                        }, $courses));
         $term = $courses[0]->term;
 
         if (empty($srs_list) || empty($term)) {
-            // course somehow got here and did not have term/srs, so skip it
+            // Course somehow got here and did not have term/srs, so skip it.
             return grade_reporter::NOTSENT;
         }
 
-        // If this is a crosslisted course, find out through what SRS he/she 
-        // enrolled in.  This info is in the ccle_roster_class_cache table
+        // If this is a crosslisted course, find out through what SRS he/she
+        // enrolled in. This info is in the ccle_roster_class_cache table.
         $sql = "SELECT  urc.id, urc.term, urc.srs,
                         urc.subj_area, urc.crsidx, urc.secidx,
                         u.idnumber as uidstudent
-                FROM    {ccle_roster_class_cache} crcc, 
+                FROM    {ccle_roster_class_cache} crcc,
                         {ucla_reg_classinfo} AS urc,
                         {user} AS u
                 WHERE   u.id = $this->userid AND
@@ -82,21 +100,21 @@ class ucla_grade_grade extends grade_grade {
                         crcc.param_srs IN ($srs_list)";
         $enrolledcourses = $DB->get_records_sql($sql);
 
-        // We should only get one record, but we should handle multiple
+        // We should only get one record, but we should handle multiple.
         if (empty($enrolledcourses)) {
-            // user is most likely the Instructor or TA or manually added guest
-            // just skip user
+            // User is most likely the Instructor or TA or manually added guest
+            // just skip user.
             return grade_reporter::NOTSENT;
-        } 
+        }
 
-        // Want the transaction ID to be the last record in the _history table
+        // Want the transaction ID to be the last record in the _history table.
         list($transactionid, $loggeduser) =
                 grade_reporter::get_transactionid($this->table, $this->id);
 
         $transaction_user = grade_reporter::get_transaction_user($this,
                         $loggeduser);
 
-        // Prepare log variables
+        // Prepare log variables.
         $log = array(
             'itemmodule' => $this->grade_item->itemmodule,
             'itemtype' => $this->grade_item->itemtype,
@@ -107,84 +125,87 @@ class ucla_grade_grade extends grade_grade {
 
         foreach ($enrolledcourses as $course) {
             if (empty($course->uidstudent)) {
-                // ignore users with no uid
+                // Ignore users with no uid.
                 return grade_reporter::NOTSENT;
             }
 
             $param = $this->make_myucla_parameters($course, $transactionid);
             try {
-                //Connect to MyUCLA and send data
+                // Connect to MyUCLA and send data.
                 $client = grade_reporter::get_instance();
                 $result = $client->moodleGradeModify($param);
 
-                // Check for status error
+                // Check for status error.
                 if (!$result->moodleGradeModifyResult->status) {
                     throw new Exception($result->moodleGradeModifyResult->message);
                 }
 
-                // Success is logged conditionally
-                if(!empty($CFG->gradebook_log_success)) {
-                    $log['action'] = get_string('gradesuccess', 'local_gradebook');
+                // Success is logged conditionally.
+                if (!empty($CFG->gradebook_log_success)) {
+                    $log['action'] = get_string('gradesuccess',
+                            'local_gradebook');
                     $log['info'] = $result->moodleGradeModifyResult->message;
                     grade_reporter::add_to_log($log);
-                }                
-
+                }
             } catch (SoapFault $e) {
-                // Catch a SOAP failure
+                // Catch a SOAP failure.
                 $log['action'] = get_string('connectionfail', 'local_gradebook');
-                $log['info'] = get_string('gradeconnectionfailinfo', 'local_gradebook', $this->id);
+                $log['info'] = get_string('gradeconnectionfailinfo',
+                        'local_gradebook', $this->id);
                 $log['info'] .= ': ' . $e;  // Append exact SOAP error as well.
 
                 grade_reporter::add_to_log($log);
                 return grade_reporter::CONNECTION_ERROR;
             } catch (Exception $e) {
-                // Catch a 'status' failure
+                // Catch a 'status' failure.
                 $log['action'] = get_string('gradefail', 'local_gradebook');
-                $log['info'] = get_string('gradefailinfo', 'local_gradebook', $this->id);
+                $log['info'] = get_string('gradefailinfo', 'local_gradebook',
+                        $this->id);
 
                 grade_reporter::add_to_log($log);
                 return grade_reporter::BAD_REQUEST;
             }
         }
-        
+
         return grade_reporter::SUCCESS;
     }
 
     /**
      * Creates array of values to be used when creating message about
-     * grade_grades to MyUCLA
+     * grade_grades to MyUCLA.
      *
      * @global object $CFG
-     * @param object $course
+     * @param object $course    Info from ucla_reg_courseinfo and student UID.
      * @param int $transactionid
      * @return array    Returns an array to create the SOAP message that will
      *                  be sent to MyUCLA
      */
-    private function make_myucla_parameters($course, $transactionid) {
+    public function make_myucla_parameters($course, $transactionid) {
         global $CFG;
-        
-        // person who made/changed grade
+
+        // Person who made/changed grade.
         $transaction_user = grade_reporter::get_transaction_user($this);
-        
-        // Trim long feedback
+
+        // Trim long feedback.
         $comment = '';
         if (isset($this->feedback)) {
             if (strlen($this->feedback) > grade_reporter::MAX_COMMENT_LENGTH) {
-                $comment = trim(substr($this->feedback, 0,
-                        grade_reporter::MAX_COMMENT_LENGTH)) .
+                $comment = trim(textlib::substr($this->feedback, 0,
+                                        grade_reporter::MAX_COMMENT_LENGTH)) .
                         get_string('continue_comments', 'local_gradebook');
             } else {
                 $comment = trim($this->feedback);
             }
+            $comment = $this->strip_invalid_xml_characters($comment);
         }
-        
-        // Set variables to notify deletion
-        if(!empty($this->deleted)) {
+
+        // Set variables to notify deletion.
+        if (!empty($this->deleted)) {
             $this->finalgrade = null;
             $this->feedback = 'Deleted';
         }
 
-        //Create array with all the parameters and return it
+        // Create array with all the parameters and return it.
         return array(
             'mInstance' => array(
                 'miID' => $CFG->gradebook_id,
@@ -205,12 +226,50 @@ class ucla_grade_grade extends grade_grade {
             ),
             'mTransaction' => array(
                 'userUID' => empty($transaction_user->idnumber) ?
-                    '000000000' : $transaction_user->idnumber,
+                        '000000000' : $transaction_user->idnumber,
                 'userName' => fullname($transaction_user, true),
                 'userIpAddress' => empty($transaction_user->lastip) ?
-                    '0.0.0.0' : $transaction_user->lastip,
+                        '0.0.0.0' : $transaction_user->lastip,
                 'moodleTransactionID' => $transactionid,
             )
         );
     }
+
+    /**
+     * Removes invalid XML characters.
+     *
+     * ASP.NET webservices use XML 1.0 which restricts the character set allowed
+     * to the following chars:
+     *  #x9 | #xA | #xD | x20-#xD7FF | xE000-#xFFFD | x10000-#x10FFFF
+     *  (Source: http://www.w3.org/TR/REC-xml/#charsets)
+     *
+     * Function is from: http://stackoverflow.com/a/3466049/6001
+     *
+     * @param string $value
+     * @return string
+     */
+    private function strip_invalid_xml_characters($value) {
+        $ret = "";
+        $current;
+        if (empty($value)) {
+            return $ret;
+        }
+
+        $length = textlib::strlen($value);
+        for ($i=0; $i<$length; $i++) {
+            $current = ord($value{$i});
+            if (($current == 0x9) ||
+                    ($current == 0xA) ||
+                    ($current == 0xD) ||
+                    (($current >= 0x20) && ($current <= 0xD7FF)) ||
+                    (($current >= 0xE000) && ($current <= 0xFFFD)) ||
+                    (($current >= 0x10000) && ($current <= 0x10FFFF))) {
+                $ret .= chr($current);
+            } else {
+                $ret .= " ";
+            }
+        }
+        return $ret;
+    }
+
 }
