91b31cfe2fbc926b341325892360a4ada0fabd4f
Brandon Ly <brandonly@live.com>
2014-06-26 15:38:41 -0700

    SSC-2778 - Made Frontpage Image/Banner and Division List configurable
    * Added new frontpage image of Royce Hall for SSC and renamed frontpage image
      for shared system.
    * Made it so that if, in the config, the division list is empty, there would
      be no space where the divison list was.

--- theme/uclashared/lib.php
+++ theme/uclashared/lib.php
@@ -2,16 +2,22 @@
 
 /**
  * Process a CSS directive to load a font.  Currently works for Bootstrap3 glyphicons.
+ * SSC-2778: This filter now also gives choice for the frontpage image! The image
+ * is specified in the config file, where its title is set in the config variable
+ * "$CFG->forced_plugin_settings['theme_uclashared']['frontpage_image'];".
  * 
  * @global type $CFG
  * @param type $css
  * @param type $theme
  * @return type
  */
- function uclashared_process_css($css, $theme) {
+function uclashared_process_css($css, $theme) {
     global $CFG;
     $tag = '[[font:glyphicons-halflings-regular]]';
-    $replacement = $CFG->wwwroot.'/theme/uclashared/package/bootstrap/fonts/glyphicons-halflings-regular';
+    $replacement = $CFG->wwwroot . '/theme/uclashared/package/bootstrap/fonts/glyphicons-halflings-regular';
+    $css = str_replace($tag, $replacement, $css);
+    $tag = 'frontpage-image';
+    $replacement = get_config('theme_uclashared', 'frontpage_image');
     $css = str_replace($tag, $replacement, $css);
     return $css;
 }
