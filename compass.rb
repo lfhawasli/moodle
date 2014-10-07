08c19a5bfa0652cf4b5c7ff288afd56c2d358a2f
Alfonso Roman <aroman@oid.ucla.edu>
2014-10-07 16:27:14 -0700

    CCLE-4791 - fixing compass watch problems

--- compass.rb
+++ compass.rb
@@ -1,38 +1,21 @@
 
-## UCLA theme directory
-theme_dir = "theme/uclashared"
-
-## Location of CSS dir
-css_dir = "#{theme_dir}/style"
-
-## Location of SASS dir
-sass_dir = "#{theme_dir}/sass"
-
-## Location of images
-images_dir = "#{theme_dir}/pix"
-
-## Location of javascript
-javascripts_dir = "#{theme_dir}/javascript"
-
-## Import boostrap package
-additional_import_paths = ["#{theme_dir}/package"]
-
-
-# @todo 
-# Compile CSS for Moodle modules ONLY for production
-# 
-# Create a styles.css files for modules
-# 
-Dir.glob('**/sass/styles.scss').each do |f|
-   puts f
-   ## Compile compass inside compass, whoa!
-    cmd = "compass compile --sass-dir #{f.gsub(/\/styles.scss/, '')} --css-dir #{f.gsub(/\/styles.scss/, '')} -I #{sass_dir} --environment production --output-style compressed"
-    puts cmd
-    
-    ## Do system call
-    system(cmd)
-
-    cmd = "mv #{f.gsub(/\/styles.scss/, '')}/styles.css #{f.gsub(/\/sass\/styles.scss/, '')}/styles.min.css"
-    puts cmd
-    system(cmd)
+class String
+    def black;          "\033[30m#{self}\033[0m" end
+    def red;            "\033[31m#{self}\033[0m" end
+    def green;          "\033[32m#{self}\033[0m" end
+    def brown;          "\033[33m#{self}\033[0m" end
+    def blue;           "\033[34m#{self}\033[0m" end
+    def magenta;        "\033[35m#{self}\033[0m" end
+    def cyan;           "\033[36m#{self}\033[0m" end
+    def gray;           "\033[37m#{self}\033[0m" end
+    def bg_black;       "\033[40m#{self}\033[0m" end
+    def bg_red;         "\033[41m#{self}\033[0m" end
+    def bg_green;       "\033[42m#{self}\033[0m" end
+    def bg_brown;       "\033[43m#{self}\033[0m" end
+    def bg_blue;        "\033[44m#{self}\033[0m" end
+    def bg_magenta;     "\033[45m#{self}\033[0m" end
+    def bg_cyan;        "\033[46m#{self}\033[0m" end
+    def bg_gray;        "\033[47m#{self}\033[0m" end
+    def bold;           "\033[1m#{self}\033[22m" end
+    def reverse_color;  "\033[7m#{self}\033[27m" end
 end
