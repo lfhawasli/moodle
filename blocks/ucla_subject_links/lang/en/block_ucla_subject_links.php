3ce0a9b928793dfee806098a9d0344923e3d6e37
Queenie Ma <queeniema@ucla.edu>
2015-05-14 10:57:25 -0700

    CCLE-4016 - Add logging to subject area links page

--- blocks/ucla_subject_links/lang/en/block_ucla_subject_links.php
+++ blocks/ucla_subject_links/lang/en/block_ucla_subject_links.php
@@ -7,3 +7,5 @@ $string['title'] = 'Subject links';
 $string['link_text'] = '{$a} links';
 
 $string['error'] = 'Sorry, but could not find additional information for given subject area.';
+
+$string['eventpageviewed'] = 'Page viewed';
