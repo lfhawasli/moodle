5babb25a7204db31264a6fea56b939980bb5feb3
mehansen <mhansen@oid.ucla.edu>
2015-06-08 11:23:29 -0700

    CCLE-4999 - Monitor for changed SRS numbers
    * added stored procedure class for ucla_get_course_srs
    * added check to account for changed srs numbers in ucla_reg_classinfo_cron
    * added a transaction block for updating tables to ensure that ucla_reg_classinfo and ucla_request_classes stay consistent with each other

---
+++ local/ucla/registrar/registrar_ucla_get_course_srs.class.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of the UCLA local plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains stored procedure class ucla_get_course_srs.
+ * 
+ * Documentation: https://ccle.ucla.edu/mod/page/view.php?id=395287
+ *
+ * @package    local_ucla
+ * @copyright  2015 UC Regents
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/registrar_stored_procedure.base.php');
+
+/**
+ * Stored procedure ucla_get_course_srs.
+ * 
+ * @package    local_ucla
+ * @copyright  2015 UC Regents
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class registrar_ucla_get_course_srs extends registrar_stored_procedure {
+
+    /**
+     * Returns query params.
+     * 
+     * @return array    array of strings
+     */
+    function get_query_params() {
+        return array('term', 'subject area', 'crsidx', 'secidx');
+    }
+
+}
