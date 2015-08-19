4ba1f9a41b6e8a2681de5fcdae1b718c63d26591
Rex Lorenzo <rex@oid.ucla.edu>
2015-08-19 15:29:44 -0700

    CCLE-4027 - Install and evaluate Respondus
    * Initial commit.

---
+++ blocks/lockdownbrowser/block_lockdownbrowser.php
@@ -0,0 +1,68 @@
+<?php
+// Respondus LockDown Browser Extension for Moodle
+// Copyright (c) 2011-2015 Respondus, Inc.  All Rights Reserved.
+// Date: July 15, 2015.
+
+class block_lockdownbrowser extends block_base {
+
+    public function init() {
+
+        $this->content_type = BLOCK_TYPE_TEXT;
+
+        // ensure title is unique even if string table is unavailable
+        $this->title = get_string("lockdownbrowser", "block_lockdownbrowser");
+    }
+
+    public function get_content() {
+
+        global $CFG, $COURSE;
+
+        if ($this->content != null) {
+
+            return $this->content;
+        }
+
+        $this->content       = new stdClass;
+        $this->content->text = '';
+
+        if (bccomp($CFG->version, 2013111800, 2) >= 0) {
+            // Moodle 2.6.0+.
+            $context = context_course::instance($COURSE->id);
+        } else {
+            // Prior to Moodle 2.6.0.
+            $context = get_context_instance(CONTEXT_COURSE, $COURSE->id);
+        }
+
+        if (has_capability('moodle/course:manageactivities', $context)) {
+
+            $this->content->footer = '<a href="' . $CFG->wwwroot . '/blocks/lockdownbrowser/dashboard.php?course=' .
+                $COURSE->id . '">' . get_string('dashboard', 'block_lockdownbrowser') . ' ...</a>';
+        } else {
+            $this->content->footer = '';
+        }
+
+        return $this->content;
+    }
+
+    public function instance_allow_multiple() {
+
+        return false;
+    }
+
+    public function applicable_formats() {
+
+        return array(
+            'site-index'         => false,
+            'course-view'        => true,
+            'course-view-social' => false,
+            'mod'                => false,
+            'mod-quiz'           => false
+        );
+    }
+
+    public function has_config() {
+
+        return true;
+    }
+}
+
---
+++ blocks/lockdownbrowser/version.php
@@ -0,0 +1,9 @@
+<?php
+// Respondus LockDown Browser Extension for Moodle
+// Copyright (c) 2011-2015 Respondus, Inc.  All Rights Reserved.
+// Date: July 15, 2015.
+
+// internal version
+$plugin->version  = 2015071500; // the current plugin version (yyyymmddxx)
+$plugin->requires = 2010122500; // requires this Moodle version (2.0.1+)
+
