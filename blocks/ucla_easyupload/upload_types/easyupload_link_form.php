56cc81037fb4ee12dd813a988ca8b2ba2acc77c8
Alex Wang <alexlw92@yahoo.com>
2015-09-01 12:17:23 -0700

    CCLE-4875 - Set default behavior for Add a Link to class pages *removed trailing whitespace *fixed inline comment *only gets the display value from config instead of all the url values *put the set and get default in one line

--- blocks/ucla_easyupload/upload_types/easyupload_link_form.php
+++ blocks/ucla_easyupload/upload_types/easyupload_link_form.php
@@ -9,12 +9,15 @@ class easyupload_link_form extends easy_upload_form {
     function specification() {
         $mform =& $this->_form;
 
-        $mform->addElement('url', 'externalurl', 
+        $mform->addElement('url', 'externalurl',
             get_string('dialog_add_link_box', self::associated_block), 
             array('size' => 60), array('usefilepicker' => false));
         $mform->setType('externalurl', PARAM_URL);
 
-        $mform->addRule('externalurl', null, 'required');        
+        $mform->addRule('externalurl', null, 'required');
+
+        // Set default URL redirection.
+        $mform->setDefault('display', get_config('url', 'display'));
     }
 
     function get_coursemodule() {
