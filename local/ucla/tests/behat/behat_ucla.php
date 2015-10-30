0b4e8547f7961e21d8b95369f16eccddf603dfdc
Michelle Doelling <medoelling@gmail.com>
2015-10-29 17:41:08 -0700

    CCLE-5364 - removed Behat step 'I put a breakpoint' as it is already a Moodle core step

--- local/ucla/tests/behat/behat_ucla.php
+++ local/ucla/tests/behat/behat_ucla.php
@@ -331,25 +331,6 @@ class behat_ucla extends behat_files {
     }
 
     /**
-     * Pauses the scenario until the user presses a key. 
-     * Useful when debugging a scenario. 
-     * 
-     * @Given /^I put a breakpoint$/
-     */
-    public function i_put_a_breakpoint() {
-
-        fwrite(STDOUT,
-                "\033[s \033[93m[Breakpoint] Press \033[1;93m[RETURN]\033[0;93m to continue...\033[0m");
-
-        while (fgets(STDIN, 1024) == '') {
-            // Intentionally empty.
-        }
-
-        fwrite(STDOUT, "\033[u");
-        return;
-    }
-
-    /**
      * Based off core step i_upload_file_to_filepicker, but it doesn't wait for
      * the file picker to return successfully.
      *
