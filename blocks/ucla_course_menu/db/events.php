6a569fc944a5f0f0621b57c95e4120f4106bc6e9
Danny Jung <dannyjung90@gmail.com>
2016-07-06 15:12:37 -0700

    CCLE-5978 - Update events for block_ucla_course_menu

--- blocks/ucla_course_menu/db/events.php
+++ blocks/ucla_course_menu/db/events.php
@@ -1,6 +1,25 @@
 <?php
-/*
- * Definition of event triggers related to site menu block
+// This file is part of the UCLA course menu block for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Event handlers.
+ * 
+ * @package    block_ucla_course_menu
+ * @copyright  2016 UC Regent
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 /* Course Creation/Restore:
@@ -8,15 +27,14 @@
  *    left-hand side of the screen
  */
 
-$handlers = array (
-    'course_created' => array (
-         'handlerfile'      => '/blocks/ucla_course_menu/eventlib.php',
-         'handlerfunction'  => 'move_site_menu_block',
-         'schedule'         => 'instant'
-     ),
-    'course_restored' => array (
-         'handlerfile'      => '/blocks/ucla_course_menu/eventlib.php',
-         'handlerfunction'  => 'move_site_menu_block',
-         'schedule'         => 'instant'
-     ),    
+$observers = array(
+    array(
+        'eventname' => '\core\event\course_created',
+        'callback'  => 'block_ucla_course_menu_observer::move_course_menu_block_created'
+    ),
+    
+    array(
+        'eventname' => '\core\event\course_restored',
+        'callback'  => 'block_ucla_course_menu_observer::move_course_menu_block_restored'
+    )
 );
