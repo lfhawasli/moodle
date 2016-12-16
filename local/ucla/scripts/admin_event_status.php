47fb526cd0a7ed24713c72af274dd1e4c9eaf59f
Rex Lorenzo <rex@oid.ucla.edu>
2016-12-15 16:38:47 -0800

    CCLE-6427 - Script to email admins when assignment feedback pdf queue is not being processed CCLE-6428 - Script to email admins when forum queue is not being processed

--- local/ucla/scripts/admin_event_status.php
+++ local/ucla/scripts/admin_event_status.php
@@ -1,19 +1,36 @@
 <?php
-// CCLE-3794
-// Script to email admins when event queue is not being processed
+// This file is part of the UCLA local plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Script to email admin when certain processing queues are not being processed.
+ *
+ * @package    local_ucla
+ * @copyright  2016 UCLA regents
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-// Satisfy Moodle's requirement for running CLI scripts
 define('CLI_SCRIPT', true);
 
-$moodleroot = dirname(dirname(dirname(dirname(__FILE__)))); 
-require($moodleroot . '/config.php');
-require($moodleroot . '/local/ucla/lib.php');
+require(__DIR__.'/../../../config.php');
+require($CFG->dirroot . '/local/ucla/lib.php');
 require($CFG->libdir . '/clilib.php');
 
-// Support a 'tolerance' param
-// Will allow admin to set a value threshold for retry count
-list($ext_argv, $unrecog) = cli_get_params(
+// Support a 'tolerance' param.
+// Will allow admin to set a value threshold for retry count.
+list($extargv, $unrecog) = cli_get_params(
     array(
         'tolerance' => false,
         'maxcount' => false,
@@ -24,70 +41,45 @@ list($ext_argv, $unrecog) = cli_get_params(
     )
 );
 
-// Default values
-$default_tolerance = 5;
-$default_display = 20;
+// Default values.
+$defaulttolerance = 5;  // How big can the faildelay be in minutes?
+$defaultdisplay = 20;
 $defaultmaxcount = 30;
 
-$tolerance = (!empty($ext_argv['tolerance']) && !empty($unrecog[0]))
-    ? $unrecog[0] : $default_tolerance;
+$tolerance = (!empty($extargv['tolerance']) && !empty($unrecog[0])) ? $unrecog[0] : $defaulttolerance;
+$maxcount = (!empty($extargv['maxcount']) && !empty($unrecog[1])) ? $unrecog[1] : $defaultmaxcount;
 
-$maxcount = (!empty($ext_argv['maxcount']) && !empty($unrecog[1]))
-    ? $unrecog[1] : $defaultmaxcount;
+// Queues to monitor.
+$queues = array('assignfeedback_editpdf_queue' => '\assignfeedback_editpdf\task\convert_submissions',
+    'forum_queue' => '\mod_forum\task\cron_task',
+    'events_queue_handlers' => '\core\task\events_cron_task');
 
-try {
-
-    // Find records with the 'status' count that's greater than the tolerance
-    $records = $DB->get_records_select('events_queue_handlers', 'status >= :limit',
-            array('limit' => $tolerance));
-
-    // Get count of event records.
-    $totalrecords = $DB->count_records('events_queue_handlers');
-
-    // If we find such records, notify admins
-    if(!empty($records) || $totalrecords > $maxcount) {
-
-        $out = "";
-        if (!empty($records)) {
-            $count = count($records);
-            // Only display a small sampling.  Event queue backlog can grow
-            // rather large.
-            if($count <= $default_display) {
-                foreach($records as $r) {
-                    $out .= json_encode($r, JSON_PRETTY_PRINT);
-                    $out .= "\n\n";
-                }
-            } else {
-                $out = "There are more than $default_display records to display!";
-            }
+$subject = "Problems with queues: ";
+$message = "";
+foreach ($queues as $queue => $task) {
+    // Find number of records for a given queue.
+    $totalrecords = $DB->count_records($queue);
+    if ($totalrecords > $maxcount) {
+        // Check to see if the task that processes the queue is failing.
+        $alert = $DB->record_exists_select('task_scheduled', 'classname=? AND faildelay>?',
+                array($task, $tolerance * MINSECS));
+        if (!empty($alert)) {
+            $subject .= "$queue ";
+            $message .= "There are $totalrecords pending events in the "
+                . "$queue queue. The task that processes that queue has a "
+                . "faildelay greater than the tolerance of $tolerance minutes.\n\n";
         }
-
-        // Prepare message.
-        $message = '';
-        if (!empty($records)) {
-            $message = "There are $count failed events in the queue that have been retried more than $tolerance times: \n";
-            $message .= "------------------\n";
-            $message .= $out;
-        }
-        if ($totalrecords > $maxcount) {
-            $message = "There are $totalrecords total events in the queue, which is more than our threshold of $maxcount.\n";
-        }
-
-        $subject = "Total events in queue: " . $totalrecords;
-        $to = get_config('local_ucla', 'admin_email');
-        if (empty($to)) {
-            // variable not properly set
-            cli_error("Event queue check: Error -- you're missing the 'to' email field!");
-        }
-
-        cli_problem("Event queue check: Event queue has grown too much, email sent");
-        return ucla_send_mail($to, $subject, $message);
     }
-    
-    echo "Event queue check: Successfully ran the script\n";
-    return 0;
-    
-} catch (Exception $e) {
-    // DB error
-    cli_error("Event queue check: There was an error in the script.");
 }
+
+// If we find any queue delays, notify admins.
+if (!empty($message)) {
+    $to = get_config('local_ucla', 'admin_email');
+    if (empty($to)) {
+        // Variable not properly set.
+        cli_error("Event queue check: Error -- you're missing the 'to' email field!");
+    }
+    cli_problem("Event queue check: Event queue has grown too much, email sent");
+    return ucla_send_mail($to, $subject, $message);
+}
+cli_writeln("Event queue check: Successfully ran the script");
