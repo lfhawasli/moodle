e4792ffdf33c1330cfe83b243b05d82187acdd63
Abdullah Albanyan <abalbanyan@hotmail.com>
2017-08-17 10:31:41 -0700

    CCLE-6868 - Revamp date picker using Flatpickr.

--- lib/form/datetimeselector.php
+++ lib/form/datetimeselector.php
@@ -52,7 +52,18 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
      *      Note: dst will be calculated for string timezones only
      *      {@link http://docs.moodle.org/dev/Time_API#Timezone}
      * step => step to increment minutes by
+     * START UCLA MOD: CCLE-6768 - Revamp date picker.
+     * placeholder => placeholder text to place in the input field for the calendar
+     * time24hr => boolean, if true, displays the time in 24-hour format. Defaults to user
+     *                      preference.
+     * timeformat => string, the time format. Defaults to site/user preference.
+     * locale => string, affects the language of the selector. Defaults to current language.
+     * clear => boolean, if true adds a "clear" button next to the flatpickr, which
+     *              resets the flatpickr to 0. If false, the button will not show, and the
+     *              flatpickr will submit defaulttime if it is cleared with backspace or DEL.
      * optional => if true, show a checkbox beside the date to turn it on (or off)
+     *              (Note - this option does not work with moodleform::repeat_elements())
+     * END UCLA MOD: CCLE-6768.
      * @var array
      */
     protected $_options = array();
@@ -80,8 +91,22 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
     public function __construct($elementName = null, $elementLabel = null, $options = array(), $attributes = null) {
         // Get the calendar type used - see MDL-18375.
         $calendartype = \core_calendar\type_factory::get_calendar_instance();
+        // START UCLA MOD: CCLE-6868 - Revamp date picker.
+        /*
         $this->_options = array('startyear' => $calendartype->get_min_year(), 'stopyear' => $calendartype->get_max_year(),
             'defaulttime' => 0, 'timezone' => 99, 'step' => 5, 'optional' => false);
+        */
+        // Get locale default timeformat and check if it is in 24 or 12 hour format.
+        $timeformat = get_string('strftimetime');
+        $time24hr = true;
+        if (strpos($timeformat, '%p')) {
+            $time24hr = false;
+        }
+        // Note: flatpickr does not support non-gregorian calendar.
+        $this->_options = array('startyear' => $calendartype->get_min_year(), 'stopyear' => $calendartype->get_max_year(),
+                'defaulttime' => time(), 'timezone' => 99, 'step' => 1, 'optional' => false, 'clear' => false,
+                'placeholder' => 'Select date...', 'time24hr' => $time24hr, 'timeformat' => $timeformat, 'locale' => current_language());
+        // END UCLA MOD: CCLE-6868.
 
         // TODO MDL-52313 Replace with the call to parent::__construct().
         HTML_QuickForm_element::__construct($elementName, $elementLabel, $attributes);
@@ -101,10 +126,14 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
             }
         }
 
+        // START UCLA MOD: CCLE-6868 - Revamp date picker.
+        /*
         // The YUI2 calendar only supports the gregorian calendar type.
         if ($calendartype->get_name() === 'gregorian') {
             form_init_date_js();
         }
+        */
+        // END UCLA MOD: CCLE-6868.
     }
 
     /**
@@ -117,14 +146,98 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
         self::__construct($elementName, $elementLabel, $options, $attributes);
     }
 
+    // START UCLA MOD: CCLE-6868 - Revamp date picker.
+    /**
+     * Utility function to translate PHP strftime date/time tokens to the type of
+     * JavaScript tokens used by flatpickr.
+     * @param $datestring string php strftime formatted datetime string
+     * @return string
+     */
+    function translate_date_tokens($datestring) {
+        return strtr($datestring, array(
+            '%' => '',  'a' => 'D', 'A' => 'l', 'e' => 'j',
+            'j' => '',  'u' => 'w', 'U' => '',  'V' => '',
+            'W' => '',  'b' => 'M', 'B' => 'M', 'h' => 'M',
+            'Z' => '',  'g' => 'y', 'G' => 'Y', 'k' => 'H',
+            'I' => 'h', 'L' => 'h', 'M' => 'i', 'P' => 'K',
+            'p' => 'K', 's' => 'U', 'z' => '',  'Z' => '',
+            'n' => '',  't' => ' ',
+            'r' => 'h:i:S K', 'R' => 'H:i',
+            'T' => 'H:i:S',   'x' => 'h:i:S K',
+            'X' => 'h:i:S K', 'c' => 'h:i:S K',
+            'D' => 'm/d/y',   'F' => 'Y-m-d'
+        ));
+    }
+    // END UCLA MOD: CCLE-6868.
+
     /**
      * This will create date group element constisting of day, month and year.
      *
      * @access private
      */
     function _createElements() {
-        global $OUTPUT;
+        // START UCLA MOD: CCLE-6868 - Revamp date picker.
+        global $OUTPUT, $PAGE;
+        $PAGE->requires->js('/lib/flatpickr/flatpickr.min.js');
+        // Custom CSS for flatpickr is added in theme/uclashared/styles/.
 
+        // Support different locales.
+        if ($this->_options['locale'] != 'en') {
+            try {
+                $PAGE->requires->js('/lib/flatpickr/l10n/'. $this->_options['locale'] .'.js');
+            } catch (moodle_exception $e) {
+                // If language is not found, fall back on en.
+                $this->_options['locale'] = 'en';
+            }
+        }
+
+        $inputname = $this->getName();
+        $time24hr = $this->_options['time24hr'] ? 'true' : 'false';
+        $dateformat = get_string('strftimedate', 'langconfig');
+        $timeformat = $this->_options['timeformat'];
+        $datetimeformat = $dateformat . ', ' . $timeformat;
+        $placeholder = (isset($this->_options['placeholder'])) ? $this->_options['placeholder'] : 'Select date...';
+        // For compatibility with names that characters like [] created by moodleform functions such
+        // as repeat_elements, we use getElementsByName instead of getElementsbyId, since HTML ids cannot contain [].
+        $flatpickrdefinition = '
+            flatpickr(document.getElementsByName("'. $inputname .'_flatpickr")[0], {
+                enableTime: true,
+                dateFormat: "U",
+                altInput: true,
+                altFormat: "'. self::translate_date_tokens($datetimeformat) .'",
+                wrap: true,
+                time_24hr: '. $time24hr . ',
+                locale: "'. $this->_options['locale'] .'",
+                minuteIncrement: ' . $this->_options['step'] . ',
+                minDate: new Date('. $this->_options['startyear'] .' , 0, 1),
+                maxDate: new Date('. $this->_options['stopyear'] .' , 11, 31),
+                onReady: function(dateObj, dateStr, fp) {
+                    fp.altInput.name = "'.$inputname .'_flatpickr_display";
+                }
+            });';
+        $this->_elements[] = @MoodleQuickForm::createElement('html',
+                '<div style="display: inline; margin-right: 10px;" class="flatpickr" name ="'. $inputname .'_flatpickr">');
+        $this->_elements[] = @MoodleQuickForm::createElement('text', 'date_time_selector', '',
+                array('data-input' => 'data-input', 'placeholder' => $placeholder));
+        $this->_elements[] = @MoodleQuickForm::createElement('static', 'flatpickrscript', '',
+            '<a style="text-decoration: none;" class="input-button" title="Calendar" data-toggle>
+                <i class="fa fa-calendar" aria-hidden="true"></i>
+            </a>' .
+            // Only display the clear button if it is enabled.
+            ($this->_options['clear'] ? 
+            '<a style="text-decoration: none;" class="input-button" title="Clear" data-clear>
+                <i class="fa fa-eraser" aria-hidden="true"></i>
+            </a>' : '') . '
+            </div>
+            <script>
+                if (window.addEventListener) {
+                    window.addEventListener("load", function(){'. $flatpickrdefinition .'});
+                } else {
+                    // Handle IE8 and below.
+                    window.attachEvent("onload", function(){'. $flatpickrdefinition .'});
+                }
+            </script>');
+        /*
         // Get the calendar type used - see MDL-18375.
         $calendartype = \core_calendar\type_factory::get_calendar_instance();
 
@@ -162,6 +275,8 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                     null, '#', $image,
                     array('class' => 'visibleifjs'));
         }
+        */
+        // END UCLA MOD: CCLE-6868.
         // If optional we add a checkbox which the user can use to turn if on
         if ($this->_options['optional']) {
             $this->_elements[] = @MoodleQuickForm::createElement('checkbox', 'enabled', null, get_string('enable'), $this->getAttributes(), true);
@@ -205,6 +320,8 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                     }
                 }
                 if (!is_array($value)) {
+                    // START UCLA MOD: CCLE-6868 - Revamp date picker.
+                    /*
                     $calendartype = \core_calendar\type_factory::get_calendar_instance();
                     $currentdate = $calendartype->timestamp_to_date_array($value, $this->_options['timezone']);
                     // Round minutes to the previous multiple of step.
@@ -215,6 +332,10 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                         'day' => $currentdate['mday'],
                         'month' => $currentdate['mon'],
                         'year' => $currentdate['year']);
+                    */
+                    $value -= $value % ($this->_options['step'] * 60); // Round value to previous multiple of step.
+                    $value = array('date_time_selector' => $value);
+                    // END UCLA MOD: CCLE-6868.
                     // If optional, default to off, unless a date was provided.
                     if ($this->_options['optional']) {
                         $value['enabled'] = $requestvalue != 0;
@@ -227,6 +348,8 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                 }
                 break;
             case 'createElement':
+                // START UCLA MOD: CCLE-6868 - Revamp date picker.
+                /*
                 if ($arg[2]['optional']) {
                     // When using the function addElement, rather than createElement, we still
                     // enter this case, making this check necessary.
@@ -240,6 +363,14 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                         $caller->disabledIf($arg[0], $arg[0] . '[enabled]');
                     }
                 }
+                */
+                // When using the function addElement, rather than createElement, we still
+                // enter this case, making this check necessary.
+                if (isset($arg[2]['optional']) && !$this->_usedcreateelement) {
+                    $caller->disabledIf($arg[0] . '[date_time_selector]', $arg[0] . '[enabled]');
+                    $caller->disabledIf($arg[0] . '_flatpickr_display', $arg[0] . '[enabled]');
+                }
+                // END UCLA MOD: CCLE-6868.
                 return parent::onQuickFormEvent($event, $arg, $caller);
                 break;
             case 'addElement':
@@ -306,6 +437,8 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                     return $this->_prepareValue(0, $assoc);
                 }
             }
+            // START UCLA MOD: CCLE-6868 - Revamp date picker.
+            /*
             // Get the calendar type used - see MDL-18375.
             $calendartype = \core_calendar\type_factory::get_calendar_instance();
             $gregoriandate = $calendartype->convert_to_gregorian($valuearray['year'],
@@ -321,7 +454,18 @@ class MoodleQuickForm_date_time_selector extends MoodleQuickForm_group {
                                                       0,
                                                       $this->_options['timezone'],
                                                       true);
+            */
 
+            $value = $valuearray['date_time_selector'];
+            // Validate $value. The user could have modified it in-browser.
+            $value -= $value % ($this->_options['step'] * 60);
+            $mincheck = strtotime('1' . ' January ' . $this->_options['startyear']) > $value;
+            $maxcheck = strtotime('1' . ' January ' . $this->_options['stopyear']) < $value;
+            // Silently set value to default if it failed validation.
+            if (empty($value) || $mincheck || $maxcheck) {
+                $value = null;
+            }
+            // END UCLA MOD: CCLE-6868.
             return $this->_prepareValue($value, $assoc);
         } else {
             return null;
