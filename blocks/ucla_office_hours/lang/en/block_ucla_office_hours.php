846ab21cb2aabee1c42bc0ff79c6747f5a5da119
Abdullah Albanyan <abalbanyan@hotmail.com>
2017-08-18 11:51:53 -0700

    CCLE-6416 - UI improvements for office hours block.

--- blocks/ucla_office_hours/lang/en/block_ucla_office_hours.php
+++ blocks/ucla_office_hours/lang/en/block_ucla_office_hours.php
@@ -19,9 +19,9 @@ $string['header'] = 'Update office hours and contact info for {$a}';
 
 $string['header_office_info'] = 'Office information';
 $string['f_officehours'] = 'Office hours';
-$string['f_officehours_text'] = 'Include the times and days of your office hours';
+$string['f_officehours_text'] = 'Include the times and days of your office hours.';
 $string['f_office'] = 'Office location';
-$string['f_office_text'] = 'Enter your campus address or office hours meeting location';
+$string['f_office_text'] = 'Enter your campus address or office hours meeting location.';
 $string['office_info_all_courses'] = 'For <span class="warning-text">ALL</span> courses ({$a})';
 
 $string['header_contact_info'] = 'Contact information';
@@ -29,7 +29,7 @@ $string['f_email_of_record'] = 'Email of record';
 $string['f_email_of_record_empty'] = 'No email of record found';
 $string['f_email'] = 'Alternative email';
 $string['f_email_display'] = 'Email of record display settings';
-$string['f_email_text'] = '(for display purposes only - course emails will still be sent to "{$a}")';
+$string['f_email_text'] = 'For display purposes. Course emails will still be sent to "{$a}", but this email will be displayed on the course.';
 $string['f_phone'] = 'Phone number';
 $string['f_phone_text'] = 'Enter a number where students can reach you or leave a message. This number will be public.';
 $string['f_website'] = 'Personal website';
