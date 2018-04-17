f2b38cdedf2116b63fcf565fef5c9cf5b226ba51
adrayv <draym001@ucla.edu>
2018-04-17 15:49:41 -0700

    CCLE-7413 - created new front page layout

--- theme/uclashared/config.php
+++ theme/uclashared/config.php
@@ -45,7 +45,7 @@ $THEME->enable_dock = false;
 // YUI CSS we want to include (we don't want any!).
 $THEME->yuicssmodules = array();
 
-// Most themes will use this rendererfactory as this is the one that allows the 
+// Most themes will use this rendererfactory as this is the one that allows the
 // theme to override any other renderer.
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 
@@ -63,5 +63,10 @@ $THEME->layouts['mydashboard']['options']['noheader'] = true;
 
 // Add new SASS styles to an include file in theme/uclashared/scss/moodle.scss.
 $THEME->scss = 'moodle';
+
 // Add javascript files.
 $THEME->javascripts[] = 'help_feedback';
+
+$THEME->layouts['frontpage'] = array(
+  'file' => 'frontpage.php',
+);
