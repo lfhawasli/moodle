f98ef0bf3b7334ab5eb08e51640b3d59f5b952ea
Tim Nguyen <timothynguyen314@users.noreply.github.com>
2018-05-03 15:34:11 -0700

    MDL-61342 mod_choice: Anonymous not truly anonymous

--- mod/choice/lang/en/choice.php
+++ mod/choice/lang/en/choice.php
@@ -118,7 +118,10 @@ $string['publish'] = 'Publish results';
 $string['publishafteranswer'] = 'Show results to students after they answer';
 $string['publishafterclose'] = 'Show results to students only after the choice is closed';
 $string['publishalways'] = 'Always show results to students';
-$string['publishanonymous'] = 'Publish anonymous results, do not show student names';
+// START UCLA MOD: CCLE-7791 - Choice: Anonymous not truly anonymous
+$string['publishanonymous'] = 'Publish anonymous results, do not show student names to students only';
+$string['publishanonymoustoall'] = 'Publish anonymous results, do not show student names';
+// END UCLA MOD: CCLE-7791
 $string['publishnames'] = 'Publish full results, showing names and their choices';
 $string['publishnot'] = 'Do not publish results to students';
 $string['removemychoice'] = 'Remove my choice';
