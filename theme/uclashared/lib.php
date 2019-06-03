530c8227eed48f16ce8f811b92b256e54b29fbdf
Rex Lorenzo <rex@oid.ucla.edu>
2019-06-03 16:46:09 -0700

    CCLE-8382 - Missing icons in gear dropdown

--- theme/uclashared/lib.php
+++ theme/uclashared/lib.php
@@ -73,7 +73,8 @@ function theme_uclashared_get_fontawesome_icon_map() {
          'atto_poodll:video' => 'fa-video-camera',
          'atto_poodll:whiteboard' => 'fa-pencil',
          'atto_poodll:snapshot' => 'fa-camera',
-         'atto_fontfamily:icon' => 'fa-font'
+         'atto_fontfamily:icon' => 'fa-font',
+         'core:i/navigationitem' => 'fa-cog',
      ];
 }
 
