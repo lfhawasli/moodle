b3217fc2c62a0839f632944defc1265c65b62498
Lillian Hawasli <lfhawasli@gmail.com>
2020-05-04 16:13:15 -0700

    CCLE-9210 - Display menu link items in nav and launch

--- mod/lti/view.php
+++ mod/lti/view.php
@@ -56,6 +56,7 @@ $l  = optional_param('l', 0, PARAM_INT);  // lti ID.
 $forceview = optional_param('forceview', 0, PARAM_BOOL);
 $ltitypeid = optional_param('ltitypeid', 0, PARAM_INT);
 $courseid = optional_param('course', 0, PARAM_INT);
+$menulinkid = optional_param('menulinkid', 0, PARAM_INT);
 
 $cm = null;
 $pageparams = array();
@@ -66,9 +67,9 @@ if ($ltitypeid && $courseid) {
     $lti->showdescriptionlaunch = false;
     $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
     $context = context_course::instance($courseid);
-    $pageparams = array('ltitypeid' => $ltitypeid, 'courseid' => $courseid);
-    $launchparam = 'ltitypeid=' . $ltitypeid . '&courseid=' . $courseid;
-    
+    $pageparams = array('ltitypeid' => $ltitypeid, 'courseid' => $courseid, 'menulinkid' => $menulinkid);
+    $launchparam = 'ltitypeid=' . $ltitypeid . '&courseid=' . $courseid . '&menulinkid=' . $menulinkid;
+
     if (is_guest($context, $USER) || !isloggedin()) {
         throw new moodle_exception('guestsarenotallowed', 'error');
     }
