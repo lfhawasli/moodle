9a590c90ab575a29da96b3f79bb6e31f965c2b64
Lillian Hawasli <lfhawasli@gmail.com>
2020-06-17 10:03:46 -0700

    Fix version number

--- lib/editor/atto/plugins/lti/version.php
+++ lib/editor/atto/plugins/lti/version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2018041004;
+$plugin->version    = 2020061504;
 $plugin->requires   = 2017110800;
 $plugin->component  = 'atto_lti';
