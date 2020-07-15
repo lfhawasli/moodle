8599c0bda10bc3dc50bd3bf95f4df3c75b89effa
Lillian Hawasli <lfhawasli@gmail.com>
2020-07-15 15:38:20 -0700

    CCLE-9396 - Fixed install.xml field types in lti_types table

--- mod/lti/db/upgrade.php
+++ mod/lti/db/upgrade.php
@@ -264,7 +264,7 @@ function xmldb_lti_upgrade($oldversion) {
 
         $fields = [];
         $fields[] = new xmldb_field('asrichtexteditorplugin', XMLDB_TYPE_INTEGER, 1, null, null, null, 0, 'asmenulink');
-        $fields[] = new xmldb_field('richtexteditorurl', XMLDB_TYPE_TEXT, 255, null, false, null, null, 'asrichtexteditorplugin');
+        $fields[] = new xmldb_field('richtexteditorurl', XMLDB_TYPE_TEXT, 'small', null, false, null, null, 'asrichtexteditorplugin');
         foreach ($fields as $field) {
             if (!$dbman->field_exists($table, $field)) {
                 $dbman->add_field($table, $field);
